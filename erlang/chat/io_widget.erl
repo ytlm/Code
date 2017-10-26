7b8f9617b1a1d68aebfaafc74c2ca9b0d0914ecc
ytlm <ytlm.lv@gmail.com>
2017-10-26 15:31:00 +0800

    update Makefile and widget

--- erlang/chat/io_widget.erl
+++ erlang/chat/io_widget.erl
@@ -1,101 +1,105 @@
 -module(io_widget).
+
 -export([get_state/1,
-         start/1, test/0,
-         set_handler/2,
-         set_prompt/2,
-         set_state/2,
-         set_title/2, insert_str/2, update_state/3]).
+     start/1, test/0,
+     set_handler/2,
+     set_prompt/2,
+     set_state/2,
+     set_title/2, insert_str/2, update_state/3]).
 
 start(Pid) ->
-    wx:new(),
+    gs:start(),
     spawn_link(fun() -> widget(Pid) end).
 
-get_state(Pid)           -> rpc(Pid, get_state).
-set_title(Pid, Str)      -> Pid ! {title, Str}.
-set_handler(Pid, Fun)    -> Pid ! {handler, Fun}.
-set_prompt(Pid, Str)     -> Pid ! {prompt, Str}.
-set_state(Pid, State)    -> Pid ! {state, State}.
-insert_str(Pid, Str)     -> Pid ! {insert, Str}.
-update_state(Pid, N, X)  -> Pid ! {updateState, N, X}.
+get_state(Pid)          -> rpc(Pid, get_state).
+set_title(Pid, Str)     -> Pid ! {title, Str}.
+set_handler(Pid, Fun)   -> Pid ! {handler, Fun}.
+set_prompt(Pid, Str)    -> Pid ! {prompt, Str}.
+set_state(Pid, State)   -> Pid ! {state, State}.
+insert_str(Pid, Str)    -> Pid ! {insert, Str}.
+update_state(Pid, N, X) -> Pid ! {updateState, N, X}.
 
 rpc(Pid, Q) ->
     Pid ! {self(), Q},
     receive
-        {Pid, R} ->
-            R
+    {Pid, R} ->
+        R
     end.
 
 widget(Pid) ->
-    Size = [{width, 500}, {height, 200}],
-    Win  = wx:window(wx:new(),
-                     [{map, true}, {configure, true}, {title, "window"}|Size]),
-    wx:frame(packer, Win, [{packer_x, [{stretch, 1, 500}]},
-                           {packer_y, [{stretch, 10, 120, 100},
-                                       {stretch, 1, 15, 15}]}]),
-    wx:create(editor, editor, packer, [{pack_x, 1}, {pack_y, 1}, {vscroll, right}]),
-    wx:create(entry, entry, packer,   [{pack_x, 1}, {pack_y, 2}, {keypress, true}]),
-    wx:config(packer, Size),
+    Size = [{width,500},{height,200}],
+    Win = gs:window(gs:start(),
+            [{map,true},{configure,true},{title,"window"}|Size]),
+    gs:frame(packer, Win,[{packer_x, [{stretch,1,500}]},
+              {packer_y, [{stretch,10,100,120},
+                      {stretch,1,15,15}]}]),
+    gs:create(editor,editor,packer, [{pack_x,1},{pack_y,1},{vscroll,right}]),
+    gs:create(entry, entry, packer, [{pack_x,1},{pack_y,2},{keypress,true}]),
+    gs:config(packer, Size),
     Prompt = " > ",
-    State  = nil,
-    wx:config(entry, {insert, {0, Prompt}}),
+    State = nil,
+    gs:config(entry, {insert,{0,Prompt}}),
     loop(Win, Pid, Prompt, State, fun parse/1).
 
 loop(Win, Pid, Prompt, State, Parse) ->
     receive
-        {From, get_state} ->
-            From ! {self(), State},
-            loop(Win, Pid, Prompt, State, Parse);
-        {handler, Fun} ->
-            loop(Win, Pid, Prompt, State, Fun);
-        {prompt, Str} ->
-            wx:config(entry, {delete, {0, last}}),
-            wx:config(entry, {insert, {0, Str}}),
-            loop(Win, Pid, Str, State, Parse);
-        {state, S} ->
-            loop(Win, Pid, Prompt, S, Parse);
-        {title, Str} ->
-            wx:config(Win, [{title, Str}]),
-            loop(Win, Pid, Prompt, State, Parse);
-        {insert, Str} ->
-            wx:config(editor, {insert, {'end', Str}}),
-            scroll_to_show_last_line(),
-            loop(Win, Pid, Prompt, State, Parse);
-        {updateState, N, X} ->
-            io:format("setelemtn N = ~p, X = ~p Stte = ~p~n", [N, X, State]),
-            State1 = setelement(N, State, X),
-            loop(Win, Pid, Prompt, State1, Parse);
-        {wx, _, destroy, _, _} ->
-            io:format("Destroyed~n", []),
-            exit(windowDestroyed);
-        {wx, entry, keypress, _, ['Return'|_]} ->
-            Text = wx:read(entry, text),
-            wx:config(entry, {delete, {0, last}}),
-            wx:config(entry, {insert, {0, Prompt}}),
-            try Parse(Text) of
-                Term ->
-                    Pid ! {self(), State, Term}
-            catch
-                _:_ ->
-                    self() ! {insert, " ** bad input ** \n ** /h for help\n"}
-            end,
-            loop(Win, Pid, Prompt, State, Parse);
-        {wx, _, configure, [], [W, H, _, _]} ->
-            wx:config(packer, [{width, W}, {height, H}]),
-            loop(Win, Pid, Prompt, State, Parse);
-        {wx, entry, keypress, _, _} ->
-            loop(Win, Pid, Prompt, State, Parse);
-        Any ->
-            io:format("Discarded : ~p~n", [Any]),
-            loop(Win, Pid, Prompt, State, Parse)
+    {From, get_state} ->
+        From ! {self(), State},
+        loop(Win, Pid, Prompt, State, Parse);
+    {handler, Fun} ->
+        loop(Win, Pid, Prompt, State, Fun);
+    {prompt, Str} ->
+        %% this clobbers the line being input ...
+        %% this could be fixed - hint
+        gs:config(entry, {delete,{0,last}}),
+        gs:config(entry, {insert,{0,Str}}),
+        loop(Win, Pid, Str, State, Parse);
+    {state, S} ->
+        loop(Win, Pid, Prompt, S, Parse);
+    {title, Str} ->
+        gs:config(Win, [{title, Str}]),
+        loop(Win, Pid, Prompt, State, Parse);
+    {insert, Str} ->
+        gs:config(editor, {insert,{'end',Str}}),
+        scroll_to_show_last_line(),
+        loop(Win, Pid, Prompt, State, Parse);
+    {updateState, N, X} ->
+        io:format("setelemtn N=~p X=~p State=~p~n",[N,X,State]),
+        State1 = setelement(N, State, X),
+        loop(Win, Pid, Prompt, State1, Parse);
+    {gs,_,destroy,_,_} ->
+        io:format("Destroyed~n",[]),
+        exit(windowDestroyed);
+    {gs, entry,keypress,_,['Return'|_]} ->
+        Text = gs:read(entry, text),
+        %% io:format("Read:~p~n",[Text]),
+        gs:config(entry, {delete,{0,last}}),
+        gs:config(entry, {insert,{0,Prompt}}),
+        try Parse(Text) of
+        Term ->
+            Pid ! {self(), State, Term}
+        catch
+        _:_ ->
+            self() ! {insert, "** bad input**\n** /h for help\n"}
+        end,
+        loop(Win, Pid, Prompt, State, Parse);
+    {gs,_,configure,[],[W,H,_,_]} ->
+        gs:config(packer, [{width,W},{height,H}]),
+        loop(Win, Pid, Prompt, State, Parse);
+    {gs, entry,keypress,_,_} ->
+        loop(Win, Pid, Prompt, State, Parse);
+    Any ->
+        io:format("Discarded:~p~n",[Any]),
+        loop(Win, Pid, Prompt, State, Parse)
     end.
 
 scroll_to_show_last_line() ->
-    Size       = wx:read(editor, size),
-    Height     = wx:read(editor, height),
-    CharHeight = wx:read(editor, char_height),
-    TopRow     = Size - Height / CharHeight,
-    if TopRow > 0 -> wx:config(editor, {vscrollpos, TopRow});
-       true       -> wx:config(editor, {vscrollpos, 0})
+    Size       = gs:read(editor, size),
+    Height     = gs:read(editor, height),
+    CharHeight = gs:read(editor, char_height),
+    TopRow     = Size - Height/CharHeight,
+    if  TopRow > 0 -> gs:config(editor, {vscrollpos, TopRow});
+    true       -> gs:config(editor, {vscrollpos, 0})
     end.
 
 test() ->
@@ -103,15 +107,15 @@ test() ->
 
 test1() ->
     W = io_widget:start(self()),
-    io_widget:set_title(W, "Test Window"),
+    io_widget:set_title(W, "Test window"),
     loop(W).
 
 loop(W) ->
     receive
-        {W, {str, Str}} ->
-            Str1 = Str ++ "\n",
-            io_widget:insert_str(W, Str1),
-            loop(W)
+    {W, {str, Str}} ->
+        Str1 = Str ++ "\n",
+        io_widget:insert_str(W, Str1),
+        loop(W)
     end.
 
 parse(Str) ->
