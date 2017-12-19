12ee2b20b6ef1dd007895445aabbca181989b777
ytlm <ytlm.lv@gmail.com>
2017-12-19 09:33:23 +0800

    update ffmpeg example muxer

--- c/ffmpeg/muxer.c
+++ c/ffmpeg/muxer.c
@@ -16,24 +16,6 @@
 #include "libavutil/mathematics.h"
 #include "libswscale/swscale.h"
 
-#if 0
-void printPkt(AVPacket pkt)
-{
-    printf("AVBufferRef : %p\n"
-           "pts : %d\n"
-           "dts : %d\n"
-           "data : %p\n"
-           "size : %d\n"
-           "stream_index : %d\n"
-           "flags : %d\n"
-           "AVPacketSideData : %p\n"
-           "side_data_elems : %d\n"
-           "duration : %d\n"
-           "pos : %d\n",
-            &(pkt.buf), pkt.pts, pkt.dts, &(pkt.data), pkt.size, pkt.stream_index,
-            pkt.flags, &(pkt.side_data), pkt.side_data_elems, pkt.duration, pkt.pos);
-}
-#endif
 
 int main(int argc, char* argv[])
 {
@@ -49,25 +31,24 @@ int main(int argc, char* argv[])
     // const char* input = "/data/webroot/jljt.mpg";
     const char* output = "/tmp/test.ts";
 
-    int buffer_size = 32768;
+    // int buffer_size = 32768;
 
-	unsigned char* buffer_in = NULL; unsigned char* buffer_out = NULL;
+	// unsigned char* buffer_in = NULL;
+    // unsigned char* buffer_out = NULL;
 
 	AVFormatContext* ic = NULL;
 	AVFormatContext* oc = NULL;
 
-	AVStream* out_stream = NULL;
-	AVStream* in_stream = NULL;
+    // AVIOContext *avio_in = NULL;
+    // AVIOContext *avio_out = NULL;
 
-    AVIOContext *avio_in = NULL;
-    AVIOContext *avio_out = NULL;
-
-    AVInputFormat *ifmt = NULL;
+    // AVInputFormat *ifmt = NULL;
     AVOutputFormat *ofmt = NULL;
 
 	AVPacket pkt;
 
 	int i = 0, nRet = -1;
+    // int *stream_mapping = NULL;
 
 	av_register_all();
 	// avcodec_register_all();
@@ -100,7 +81,7 @@ int main(int argc, char* argv[])
     }
     */
 
-	// nRet = avformat_open_input(&ic, "nothing", ifmt, NULL);
+	// nRet = avformat_open_input(&ic, NULL, NULL, NULL);
 	nRet = avformat_open_input(&ic, input, NULL, NULL);
 	if (nRet != 0)
 	{
@@ -114,6 +95,14 @@ int main(int argc, char* argv[])
 		printf("Call av_find_stream_info function failed!\n");
 		return 0;
 	}
+
+    /*
+    stream_mapping = av_mallocz_array(ic->nb_streams, sizeof(*stream_mapping));
+    if (!stream_mapping) {
+        printf("av_mallocz_array() failed!\n");
+        return 1;
+    }
+    */
 
     /* init output contex */
     ofmt = av_guess_format("mpegts", NULL, NULL);
@@ -145,52 +134,80 @@ int main(int argc, char* argv[])
     oc->pb = avio_out;
     */
 
-    if (avio_open(&(oc->pb), output, AVIO_FLAG_WRITE) < 0) {
-        printf("avio_open2() failed\n");
-        return 1;
-    }
-
     /* finish output contex */
 
+    // int stream_index = 0;
+
 	for(i = 0;i < ic->nb_streams; ++i){
-		in_stream = ic->streams[i];
-		out_stream = avformat_new_stream(oc,in_stream->codec->codec);
+
+        AVStream *out_stream;
+
+        AVStream *in_stream = ic->streams[i];
+
+        AVCodecParameters *in_codecpar = in_stream->codecpar;
+
+        if (in_codecpar->codec_type != AVMEDIA_TYPE_VIDEO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
+            in_codecpar->codec_type != AVMEDIA_TYPE_SUBTITLE) {
+            // stream_mapping[i] = -1;
+            continue;
+        }
+
+        // stream_mapping[i] = stream_index++;
+
+        out_stream = avformat_new_stream(oc, NULL);
 		if(!out_stream){
 			printf("Failed allocating output Stream\n");
-			return 0;
+			return 1;
 		}
-		nRet = avcodec_copy_context(out_stream->codec,in_stream->codec);
+
+        nRet = avcodec_parameters_copy(out_stream->codecpar, in_codecpar);
 		if(nRet < 0){
 			printf("avcodec_copy_context()  failed\n");
 			return 0;
 		}
-		out_stream->codec->codec_tag = 0;
-		if(oc->oformat->flags & AVFMT_GLOBALHEADER)
-				out_stream->codec->flags |= AVFMT_GLOBALHEADER;
+
+		out_stream->codecpar->codec_tag = 0;
 	}
+
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        if (avio_open(&(oc->pb), output, AVIO_FLAG_WRITE) < 0) {
+            printf("avio_open2() failed\n");
+            return 1;
+        }
+    }
 
 	nRet = avformat_write_header(oc, NULL);
 	if(nRet < 0){
 		printf("write header failed\n");
 		return 0;
 	}
-	int frame_index = 0;
     int ss = (int)time(NULL); // date()
 
-    av_dump_format(ic, 0, NULL, 0);
+    // av_dump_format(ic, 0, NULL, 0);
 
     while(1) {
+        AVStream *in_stream, *out_stream;
+
 		nRet = av_read_frame(ic, &pkt);
 		if(nRet < 0){
             printf("av_read_frame() failed\n");
             break;
             // continue;
 		}
+
         in_stream = ic->streams[pkt.stream_index];
+        if (pkt.stream_index >= ic->nb_streams) {
+            //stream_mapping[pkt.stream_index] < 0) {
+            av_packet_unref(&pkt);
+            continue;
+        }
+        // pkt.stream_index = stream_mapping[pkt.stream_index];
+
         out_stream = ic->streams[pkt.stream_index];
 
-        printf("instream: frame rate : %d/%d, audio sample rate : %d, informat: name : %s, "
-                "pkt: pts : %d, dts : %d\n",
+        printf("aaaa instream: frame rate : %d/%d, audio sample rate : %d, informat: name : %s, "
+                "pkt: pts : %" PRId64 ", dts : %" PRId64 "\n",
                 in_stream->avg_frame_rate.num, in_stream->avg_frame_rate.den, in_stream->codecpar->sample_rate,
                 ic->iformat->name, pkt.pts, pkt.dts);
 
@@ -205,34 +222,41 @@ int main(int argc, char* argv[])
                     pkt.dts = 0;
                     ss = ss + 90000 / in_stream->codecpar->sample_rate;
                 }
-                // printf("pts : %d, dts : %d\n", pkt.pts, pkt.dts);
+                printf("0000 pts : %" PRId64 ", dts : %" PRId64 "\n", pkt.pts, pkt.dts);
             }
         }
 
-        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->codec->time_base, out_stream->codec->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
 
-        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->codec->time_base, out_stream->codec->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
 
-        nRet = av_write_frame(oc, &pkt);
+        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+
+        printf("bbbb instream: frame rate : %d/%d, audio sample rate : %d, informat: name : %s, "
+                "pkt: pts : %" PRId64 ", dts : %" PRId64 "\n",
+                in_stream->avg_frame_rate.num, in_stream->avg_frame_rate.den, in_stream->codecpar->sample_rate,
+                ic->iformat->name, pkt.pts, pkt.dts);
+
+        nRet = av_interleaved_write_frame(oc, &pkt);
         if(nRet < 0) {
-            printf("av_write_frame(0 failed\n");
+            printf("av_write_frame() failed\n");
             break;
         }
 
         av_packet_unref(&pkt);
-
-        pkt.pos = -1;
-
-        frame_index++;
 	}
 
 	av_write_trailer(oc);
 
-    avio_close(oc->pb);
+    if (oc && (oc->oformat->flags & AVFMT_NOFILE)) {
+        avio_close(oc->pb);
+    }
 
 	avformat_close_input(&ic);
 
 	avformat_free_context(oc);
+
+    // av_freep(&stream_mapping);
 
 	return 0;
 }
